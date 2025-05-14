d22529ec14aaa89d12317e5ad73dbba07aef425d
Yukikaze924 <yukikaze924@gmail.com>
2025-05-14 21:19:01 +0800

    fix: remove unnecessary div element

--- src/renderer/pages/settings/appearance.tsx
+++ src/renderer/pages/settings/appearance.tsx
@@ -10,7 +10,7 @@ export default function AppearancePage() {
         window.store.get('appearance.theme').then(setTheme);
     }, []);
 
-    if (!theme && !setThemeMode && !fetch) return null;
+    if (!theme || !setThemeMode || !fetch) return null;
 
     return (
         <div className="px-4 py-6 w-full h-main flex flex-col overflow-y-auto">
@@ -27,25 +27,23 @@ export default function AppearancePage() {
                             <h4 className="text-sm text-gray-900 dark:text-gray-50">Theme</h4>
                         </div>
                         <div className="ml-auto w-full max-w-lg min-w-[200px]">
-                            <div className="ml-auto w-full max-w-lg min-w-[200px]">
-                                <div className="relative lex items-center">
-                                    <select
-                                        className="w-full h-9.5 bg-transparent placeholder:text-slate-400 text-gray-700 dark:text-gray-300 text-sm border border-slate-200 dark:border-neutral-500 rounded-md pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-xs focus:shadow appearance-none cursor-pointer"
-                                        value={theme}
-                                        onChange={(e) => {
-                                            window.store.set('appearance.theme', e.target.value);
-                                            setTheme(e.target.value);
-                                            setThemeMode(e.target.value);
-                                            fetch();
-                                        }}
-                                    >
-                                        <option value="light">Light</option>
-                                        <option value="dark">Dark</option>
-                                    </select>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.2" stroke="currentColor" className="size-5 ml-1 absolute top-2.5 right-2.5 text-slate-700">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
-                                    </svg>
-                                </div>
+                            <div className="relative lex items-center">
+                                <select
+                                    className="w-full h-9.5 bg-transparent placeholder:text-slate-400 text-gray-700 dark:text-gray-300 text-sm border border-slate-200 dark:border-neutral-500 rounded-md pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-xs focus:shadow appearance-none cursor-pointer"
+                                    value={theme}
+                                    onChange={(e) => {
+                                        window.store.set('appearance.theme', e.target.value);
+                                        setTheme(e.target.value);
+                                        setThemeMode(e.target.value);
+                                        fetch();
+                                    }}
+                                >
+                                    <option value="light">Light</option>
+                                    <option value="dark">Dark</option>
+                                </select>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.2" stroke="currentColor" className="size-5 ml-1 absolute top-2.5 right-2.5 text-slate-700">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                                </svg>
                             </div>
                         </div>
                     </div>
