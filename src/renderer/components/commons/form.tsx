f163d5ec989338b100afb563ef2284234928acbd
Yukikaze924 <yukikaze924@gmail.com>
2025-06-07 07:40:16 +0800

    refactor: optimize component internal logic

--- src/renderer/components/commons/form.tsx
+++ src/renderer/components/commons/form.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import { FormHTMLAttributes, InputHTMLAttributes } from 'react';
-import { Input } from './input';
+import { Input, Select } from './input';
 
 type FormItemProps = {
     title?: string;
@@ -44,17 +44,13 @@ const FormSelect: React.FC<FormItemProps & {
             </div>
             <div className="w-full min-w-80">
                 <div className="relative items-center">
-                    <select
+                    <Select
                         name={name}
-                        className="w-full h-9.5 bg-transparent placeholder:text-slate-400 text-gray-700 dark:text-gray-300 text-sm border border-slate-200 dark:border-neutral-500 rounded-md pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-xs focus:shadow appearance-none cursor-pointer"
                         defaultValue={value}
                         onChange={onChange}
-                    >
-                        { placeholder && <option value={null}>{placeholder}</option> }
-                        {options.map((opt) => {
-                            return <option key={opt.value} value={opt.value}>{opt.label}</option>;
-                        })}
-                    </select>
+                        options={options}
+                        placeholder={placeholder}
+                    />
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.2" stroke="currentColor" className="size-5 ml-1 absolute top-2.5 right-2.5 text-slate-700">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                     </svg>
